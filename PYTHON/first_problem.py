d3e1da7e909337e87db76364c3bbd04e8f2987c1
jape03 <168762851+jape03@users.noreply.github.com>
2024-05-04 08:26:22 +0800

    Delete PYTHON directory

--- PYTHON/first_problem.py
+++
@@ -1,6 +0,0 @@
-# First problem
-
-width = int(input("Please enter the width of the room:  "))
-length = int(input("Please enter the length of the room: "))
-
-print(f"The area of the room is {width * length}m")
--- PYTHON/third_problem.py
+++
@@ -1,8 +0,0 @@
-# Third Problem
-
-oneLitreOrLess = int(input("How many bottles have one litre or less: "))
-moreThanOneLitre = int(input("How many bottles have more than one litre: "))
-
-refund = (oneLitreOrLess * 0.10) + (moreThanOneLitre * 0.25)
-
-print(f"The refund for returning these containers is ${refund:.2f}")
