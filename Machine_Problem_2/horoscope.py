0c9e9fd4dd1d747f4bd9250acf32c51bd1ae12e9
jape03 <japol33333@gmail.com>
2024-05-17 17:51:32 +0800

    main

--- Machine_Problem_2/horoscope.py
+++ Machine_Problem_2/horoscope.py
@@ -1,5 +1,5 @@
-# month = int(input("Please enter your month of birth as a number: "))
-# day = int(input("Please enter your day of birth as a number: "))
+month = int(input("Please enter your month of birth as a number: "))
+day = int(input("Please enter your day of birth as a number: "))
 
 zodiac_sign = ["Capricorn,Aquarius,Pisces,Aries,Taurus,Gemini,Cancer,Leo,Virgo,Libra,Scorpio,Sagittarius"]
 
