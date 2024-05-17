dee992dd081a9aad4c64f3b9b667d558a4bf5ba9
jape03 <japol33333@gmail.com>
2024-05-17 18:05:23 +0800

    mp1

--- Machine_Problem_1/second_problem.py
+++ Machine_Problem_1/second_problem.py
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-# Second Problem
 
 width = int(input("Please enter the width of the field in feet: "))
 length = int(input("Please enter the length of the field in feet: "))
 
 sqFeetPerAcre = width * length / 43560
 
-=======
-# Second Problem
-
-width = int(input("Please enter the width of the field in feet: "))
-length = int(input("Please enter the length of the field in feet: "))
-
-sqFeetPerAcre = width * length / 43560
-
->>>>>>> origin/main
 print(f"The area of the field in acres is {sqFeetPerAcre} acres.")
--- Machine_Problem_1/third_problem.py
+++ Machine_Problem_1/third_problem.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-# Third Problem
+
 
 oneLitreOrLess = int(input("How many bottles have one litre or less: "))
 moreThanOneLitre = int(input("How many bottles have more than one litre: "))
@@ -7,13 +6,4 @@ moreThanOneLitre = int(input("How many bottles have more than one litre: "))
 refund = (oneLitreOrLess * 0.10) + (moreThanOneLitre * 0.25)
 
 print(f"The refund for returning these containers is ${refund:.2f}")
-=======
-# Third Problem
 
-oneLitreOrLess = int(input("How many bottles have one litre or less: "))
-moreThanOneLitre = int(input("How many bottles have more than one litre: "))
-
-refund = (oneLitreOrLess * 0.10) + (moreThanOneLitre * 0.25)
-
-print(f"The refund for returning these containers is ${refund:.2f}")
->>>>>>> origin/main
