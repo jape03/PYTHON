8486b5a17d67f7e4e65b658626052c5e77dbdef6
jape03 <japol33333@gmail.com>
2024-05-17 17:39:21 +0800

    taestgit

---
+++ Machine_Problem_1/first_problem.py
@@ -0,0 +1,6 @@
+# First problem
+
+width = int(input("Please enter the width of the room:  "))
+length = int(input("Please enter the length of the room: "))
+
+print(f"The area of the room is {width * length}m")
